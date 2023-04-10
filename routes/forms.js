71fc37c1d3f8287491cbaf8de3cfa0b701a8c230
Muhammad Irsyadul Fikri <irsyadulfikri270418@gmail.com>
2023-04-10 20:54:01 +0700

    crud

--- routes/forms.js
+++ routes/forms.js
@@ -10,4 +10,76 @@ router.get('/', async function(req, res, next) {
   res.json(forms);
 });
 
+/* ADD forms. */
+router.post('/', async function(req, res, next) {
+  let user_id = req.body.user_id;
+  let form_id = req.body.form_id;
+  let title = req.body.title;
+  let description = req.body.description;
+  
+  await Form.create({
+    user_id: user_id,
+    form_id: form_id,
+    title: title,
+    description: description,
+  })
+  
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Ditambahkan"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+});
+
+/* UPDATE Form. */
+router.post('/:formid/edit', async function(req, res, next) {
+  // let user_id = req.params.id;
+  let form_id = req.params.formid;
+  let title = req.body.title;;
+  let description = req.body.description;;
+
+  await Form.update({
+    title: title,
+    description: description,
+  },{
+    where:{
+      form_id: form_id
+      // user_id: user_id
+    }
+  })
+
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Update"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
+});
+
+/* DELETE Form. */
+router.post('/:formid/delete', async function(req, res, next) {
+  let form_id = req.params.formid;
+  
+  await Form.destroy({
+    where: {
+      form_id: form_id
+    }
+  })
+
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Dihapus"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+});
+
 module.exports = router;
--- routes/submission.js
+++ routes/submission.js
@@ -2,12 +2,85 @@ const jwt = require('jsonwebtoken');
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
-var Subs = require('../models/submissions');
+// var Subs = require('../models/submissions');
+const Submissions = require('../models/submissions');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const submissions = await Subs.findAll();
+  const submissions = await Submissions.findAll();
   res.json(submissions);
+});
+
+/* ADD Submissions. */
+router.post('/', async function(req, res, next) {
+  let user_id = req.body.user_id;
+  let form_id = req.body.form_id;
+  let uploaded_file = req.body.uploaded_file;
+  let description = req.body.description;
+  
+  await Submissions.create({
+    user_id: user_id,
+    form_id: form_id,
+    uploaded_file: uploaded_file,
+    description: description,
+  })
+  
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Ditambahkan"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+});
+
+/* UPDATE Submissions. */
+router.post('/:formid/edit', async function(req, res, next) {
+  // let user_id = req.params.id;
+  let form_id = req.params.formid;
+  let uploaded_file = req.body.uploaded_file;;
+  let description = req.body.description;;
+
+  await Submissions.update({
+    uploaded_file: uploaded_file,
+    description: description,
+  },{
+    where:{
+      form_id: form_id
+      // user_id: user_id
+    }
+  })
+
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Update"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
+});
+
+/* DELETE submission. */
+router.post('/:formid/delete', async function(req, res, next) {
+  let form_id = req.params.formid;
+  
+  await Submissions.destroy({
+    where: {
+      form_id: form_id
+    }
+  })
+
+  .then(response =>{
+    res.json({
+      message: "Data Berhasil Dihapus"
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 });
 
 module.exports = router;
