0776893148f905d60f3bcee8256c5de4b161d3e5
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2023-06-07 16:08:33 +0700

    cors

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var formsRouter = require('./routes/forms');
-var submissionsRouter = require('./routes/submission');
-var authRouter = require('./routes/auth');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var formsRouter = require("./routes/forms");
+var submissionsRouter = require("./routes/submission");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/forms', formsRouter);
-app.use('/submissions', submissionsRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/forms", formsRouter);
+app.use("/submissions", submissionsRouter);
+app.use("/auth", authRouter);
 
 module.exports = app;
